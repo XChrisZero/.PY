0880ed7d1114f58ac3c1a1d5ac0b7804b09850e8
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-08-21 23:08:43 -0300

    in Range
    para fixar a distancia

---
+++ Start_Stop_Step.py
@@ -0,0 +1,11 @@
+# Sequência de 0 a 4 (5 não incluído)
+for i in range(5):
+    print(i)
+
+# Sequência de 2 a 9, de 2 em 2
+for i in range(2, 10, 2):
+    print(i)
+
+# Sequência decrescente de 10 a 1
+for i in range(10, 0, -1):
+    print(i)
