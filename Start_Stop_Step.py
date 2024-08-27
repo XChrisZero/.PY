280b0112551b6ddcb65841fcd753937aea4befa5
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-08-26 22:55:28 -0300

    FOR_InRange_Array

--- Start_Stop_Step.py
+++ Start_Stop_Step.py
@@ -1,14 +1,17 @@
 # Sequência de 0 a 4 (5 não incluído)
 for i in range(5):
     print(i)
+print("------------\n\n")
 
 # Sequência de 2 a 9, de 2 em 2
 for i in range(2, 10, 2):
     print(i)
+print("------------\n\n")
 
 # Sequência decrescente de 10 a 1
 for i in range(10, 0, -1):
     print(i)
+print("------------\n\n")
 
 # como não foi declarado o STEP, ele apenas incrementará 1(+1) 
 minha_lista = list(range(1, 6))
