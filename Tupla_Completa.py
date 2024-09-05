b5dc7ac2a840cd6c5672026c260a3fdb255f3d7e
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-09-05 18:49:44 -0300

    TUPLAS!
    As tuplas em Python são uma estrutura de dados IMUTAVEIS, o que significa que, uma vez criadas, seus valores não podem ser alterados. Isso significa que, se você tentar modificar uma tupla, você obterá um erro.

--- Tupla_Completa.py
+++ Tupla_Completa.py
@@ -1,34 +1,34 @@
 # Criando uma tupla
-minha_tupla = (1, 2, 3, 4, 5)
-print(minha_tupla)
+my_tpl = (1, 2, 3, 4, 5)
+print(my_tpl)
 print("------------\n\n")
 
 # Acessando elementos
-print(minha_tupla[0])  # Saída: 1
-print(minha_tupla[2])  # Saída: 3
+print(my_tpl[0])  # Saída: 1
+print(my_tpl[2])  # Saída: 3
 print("------------\n\n")
 
 # Concatenando tuplas
-tupla1 = (1, 2, 3)
-tupla2 = (4, 5, 6)
-tupla_concatenada = tupla1 + tupla2
-print(tupla_concatenada)  # Saída: (1, 2, 3, 4, 5, 6)
+tpl1 = (1, 2, 3)
+tpl2 = (4, 5, 6)
+tpl_juntas = tpl1 + tpl2
+print(tpl_juntas)  # Saída: (1, 2, 3, 4, 5, 6)
 print("------------\n\n")
 
 # Repetindo tuplas
-tupla_repetida = tupla1 * 2
-print(tupla_repetida)  # Saída: (1, 2, 3, 1, 2, 3)
+tpl_repetida = tpl1 * 2
+print(tpl_repetida)  # Saída: (1, 2, 3, 1, 2, 3)
 print("------------\n\n")
 
 # Desempacotando tuplas
-a, b, c = tupla1
+a, b, c = tpl1
 print(a)  # Saída: 1
 print(b)  # Saída: 2
 print(c)  # Saída: 3
 print("------------\n\n")
 
 # Verificando se uma tupla está vazia
-if not minha_tupla:
+if not my_tpl:
     print("A tupla está vazia")
 else:
     print("A tupla não está vazia")
