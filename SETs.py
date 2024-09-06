d8cde12d34aab1dbb954f508d3eb070c0ab01c94
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-09-05 21:46:34 -0300

    SETs
    Exercicio de criar um biscoita da sorte onde mostrará apenas uma das frases por vez

---
+++ SETs.py
@@ -0,0 +1,15 @@
+biscoito_sorte = {
+                  "A solidão é a sorte de todos os espíritos excepcionais.",
+                  "Não ser amado é falta de sorte, mas não amar é a própria infelicidade.",
+                  "Se a sua vida for a melhor coisa que já te aconteceu, acredite, você tem mais sorte do que pode imaginar.",
+                  "Eu acredito demais na sorte. E tenho constatado que, quanto mais duro eu trabalho, mais sorte eu tenho.",
+                  "A sorte não existe. Aquilo a que chamas sorte é o cuidado com os pormenores."
+                 }
+
+for biscoito in biscoito_sorte:
+  print ( biscoito )
+  break
+print ( "-----------------------------------\n\n" )
+
+print ( list ( biscoito_sorte ) [ 0 ] )  #outra forma de fazer aparecer uma das frases aleatoriamente
+print ( "___________________________________\n\n" )
