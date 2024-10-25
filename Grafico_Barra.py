ebb174ae9192c7d81d358df555d4f8aa9b28c52d
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-10-25 12:16:44 -0300

    Matplotlib
    chamando a Biblioteca Matplotlib com exemplo de grafico em barra

---
+++ Grafico_Barra.py
@@ -0,0 +1,10 @@
+#biblioteca de graficos em barras
+from matplotlib.axis import YAxis
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y,) 
+plt.show()
