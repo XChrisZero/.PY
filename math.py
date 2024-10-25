f9d97fa57b7dc1659e9678600059f3a29165db28
Christian Kuriak Chung Ferrari <161791400+XChrisZero@users.noreply.github.com>
2024-10-25 12:14:53 -0300

    Potencia/Raiz
    exemplos de potencias e raiz

---
+++ math.py
@@ -0,0 +1,10 @@
+#modulo Built-in math - para operações matematicas
+import math as mtm
+
+potencia = mtm.pow(3,3) # pow é para chamar potencia
+print ("a potencia: ")
+print (potencia)
+
+raizQuadrada = mtm.sqrt(25) # sqrt é para chamar a raiz quadrada
+print ("a raiz quadrada é : ")
+print (raizQuadrada)
